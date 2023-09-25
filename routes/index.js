c7d11d67041b051ecf3f2f0edbd2c74b63e18739
Sharina Rapales <sharinarapales@192.168.1.5>
2023-09-25 21:30:49 +0800

    Added MySQL Test

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/test-sql', function(req, res, next) {
+  res.send(process.env.MYSQL_HOST);
+});
+
 router.post('/test', function(req, res, next) {
   let users = {};
   const mysql = require('mysql')
   const connection = mysql.createConnection({
     host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT || 3306,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: 'dbmaster'
