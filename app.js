67ea73398c54e6c3ed1382c0badd8acc28f4d187
Sharina Rapales <sharinarapales@192.168.1.5>
2023-09-25 21:21:36 +0800

    Added MySQL Test

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var registerRouter = require('./routes/register');
 var loginRouter = require('./routes/login');
+require('dotenv').config()
 
 var app = express();
 
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,27 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/test', function(req, res, next) {
+  let users = {};
+  const mysql = require('mysql')
+  const connection = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: 'dbmaster'
+  })
+
+  connection.connect()
+
+  connection.query('SELECT * from my Employees', (err, rows, fields) => {
+    if (err) throw err
+
+    users = rows;
+    console.log('users', users);
+    res.send(users);
+  
+  })
+  connection.end()
+});
+
 module.exports = router;
